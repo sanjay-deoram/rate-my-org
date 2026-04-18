201648ae3c8e236388926f78539cb2cff7c3e65a
Brandon Deoram <brandondeoram@Brandons-MacBook-Pro.local>
2026-04-18 17:11:37 -0400

    fix: eslint v9 flat config structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -23,10 +24,7 @@ const eslintConfig = defineConfig([
     "next-env.d.ts",
   ]),
   // Disables ESLint rules that might conflict with prettier
-  {
-    name: "prettier",
-    extends: ["prettier"],
-  },
+  eslintConfigPrettier,
 ]);
 
 export default eslintConfig;
